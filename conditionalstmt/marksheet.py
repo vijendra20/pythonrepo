0019cea39564a9f9591d04bf13e81a4ada08c366
Vijendra Singh <vijendra.singh0682@gmail.com>
2026-06-08 23:03:57 +0530

    changes in conditional stmt

--- conditionalstmt/marksheet.py
+++ conditionalstmt/marksheet.py
@@ -2,8 +2,11 @@ hindi =23
 maths =47
 english =88
 physics =59
-chemi =60
+chemi =90
 total=hindi+maths+english+physics+chemi
+# ghaSDGvadgshgvhjadsjkhjghds
+# jhgSdbHJjgdajhvhjguhjvdsgh
+# jhbgsaadghjdknbkj
 print(total)
 avg=total/5
 print(avg)
