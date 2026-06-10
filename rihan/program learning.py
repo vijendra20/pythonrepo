48cae7e0b14ba0c32c2d7588f21a903eebd1c7b7
Vijendra Singh <vijendra.singh0682@gmail.com>
2026-06-10 11:23:40 +0530

    master

--- rihan/program learning.py
+++ rihan/program learning.py
@@ -3,3 +3,13 @@ emitax= 4500
 accesories=1200
 actualcost=laptopcost+emitax+accesories
 print(actualcost)
+
+"this is a programming calculation "
+a=400
+b=700
+c=600
+d=7800
+e=760
+total=a+b+c+d+e
+print(total)
+
