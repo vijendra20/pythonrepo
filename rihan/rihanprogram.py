757d2ebc35c90ebe862343033c4fdbb136aaa8e6
Vijendra Singh <vijendra.singh0682@gmail.com>
2026-06-10 10:57:50 +0530

    minor changes

---
+++ rihan/rihanprogram.py
@@ -0,0 +1,13 @@
+indianpopulation= 1500
+usapopulation=2300
+uaepopulation=1800
+pakpopulation=1200
+totalpopulation=indianpopulation+usapopulation+uaepopulation+pakpopulation
+print(totalpopulation)
+
+shopprofit=90000
+rent=10000
+helpersalery=12000
+electricitybjill=4500
+totalprofit=shopprofit-rent-helpersalery-electricitybjill
+print(totalprofit)
